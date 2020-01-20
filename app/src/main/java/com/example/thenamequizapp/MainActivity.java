78d4f84b8a07a5136b2d395dfbaaa1f7afc2afcf
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-20 14:33:28 +0100

    Theme changes

--- app/src/main/java/com/example/thenamequizapp/MainActivity.java
+++ app/src/main/java/com/example/thenamequizapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         List<Person> questions = new ArrayList<>();
 
