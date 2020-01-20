18c9c371ddb755a46e2efa700b84ede2da88dc26
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-20 19:23:40 +0100

    Theme changes

--- app/src/main/java/com/example/thenamequizapp/DatabaseActivity.java
+++ app/src/main/java/com/example/thenamequizapp/DatabaseActivity.java
@@ -1,14 +1,89 @@
 package com.example.thenamequizapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        fab = findViewById(R.id.fab);
+
+        if(Build.VERSION.SDK_INT >= 23){
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchPictureTakerAction();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                System.out.println(bitmap);
+            }
+        }
+    }
+
+    private void dispatchPictureTakerAction(){
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePic.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if(photoFile != null){
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(DatabaseActivity.this, "com.example.thenamequizapp.fileprovider", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+            }
+        }
+    }
+
+    private File createPhotoFile(){
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try{
+            image = File.createTempFile(name, ".jpg", storageDir);
+        }catch(Exception e){
+            Log.d("mylog", "Exception : " + e.toString());
+        }
+        return image;
     }
 }
