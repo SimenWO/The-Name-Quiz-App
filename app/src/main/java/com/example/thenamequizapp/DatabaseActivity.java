b8abe1a63893a099e903a4dab84fe9c3444f293d
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-20 11:27:02 +0100

    jauo

--- app/src/main/java/com/example/thenamequizapp/DatabaseActivity.java
+++ app/src/main/java/com/example/thenamequizapp/DatabaseActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Database extends AppCompatActivity {
+public class DatabaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
