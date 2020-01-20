4c25668185a7deb324ea2625ef32d3957153b986
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-20 12:29:49 +0100

    jauo

--- app/src/main/java/com/example/thenamequizapp/MainActivity.java
+++ app/src/main/java/com/example/thenamequizapp/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.thenamequizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    Button manageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start);
+        manageButton = findViewById(R.id.manage);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+
+
+        manageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DatabaseActivity.class));
+            }
+        });
+
     }
 
 
