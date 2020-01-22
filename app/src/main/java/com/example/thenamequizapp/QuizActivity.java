0019d58cfb4e33503ea5057aefa30da21404432e
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-22 13:29:56 +0100

    Added buttons

--- app/src/main/java/com/example/thenamequizapp/QuizActivity.java
+++ app/src/main/java/com/example/thenamequizapp/QuizActivity.java
@@ -3,12 +3,24 @@ package com.example.thenamequizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class QuizActivity extends AppCompatActivity {
+
+    EditText nameInput;
+    Button checkAnswerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        nameInput = findViewById(R.id.nameInput);
+        checkAnswerButton = findViewById(R.id.checkAnswerButton);
+
+
+
+
     }
 }
