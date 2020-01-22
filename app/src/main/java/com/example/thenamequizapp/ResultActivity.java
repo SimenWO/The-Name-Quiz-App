891c642e950fcf7c5c695ff3ae0b2ce5c9a15769
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-22 12:23:00 +0100

    Added buttons

--- app/src/main/java/com/example/thenamequizapp/ResultActivity.java
+++ app/src/main/java/com/example/thenamequizapp/ResultActivity.java
@@ -2,13 +2,37 @@ package com.example.thenamequizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ResultActivity extends AppCompatActivity {
+
+    ImageButton homeButton;
+    ImageButton restartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        homeButton = findViewById(R.id.homeButton);
+        restartButton = findViewById(R.id.restartButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, MainActivity.class));
+            }
+        });
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, QuizActivity.class));
+            }
+        });
+
     }
 }
