dfb6111b0a61db7fd9bb5013f638940edebc6921
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-23 19:15:30 +0100

    Added buttons

--- app/src/main/java/com/example/thenamequizapp/NewPersonActivity.java
+++ app/src/main/java/com/example/thenamequizapp/NewPersonActivity.java
@@ -2,13 +2,45 @@ package com.example.thenamequizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NewPersonActivity extends AppCompatActivity {
+
+
+    ImageButton imageButton;
+    Button saveButton;
+    EditText nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_person);
+
+        imageButton = findViewById(R.id.imageview);
+        saveButton = findViewById(R.id.savebutton);
+        nameText = findViewById(R.id.nameInput);
+
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
